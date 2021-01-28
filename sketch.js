9b4e0616d1c1764ab9aa4f82576095815156f1cb
rogueapo <78123378+rogueapo@users.noreply.github.com>
2021-01-27 20:21:39 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+var fixedRect,movingRect;
+
+
+function setup() {
+  createCanvas(800,400);
+  
+  fixedRect = createSprite(200,200,50,80)
+  fixedRect.shapeColor = "green"
+  movingRect = createSprite(600,200,80,50)
+  movingRect.shapeColor = "green"
+
+
+  
+}
+
+function draw() {
+  background(0); 
+  
+  movingRect.y = World.mouseY;
+  movingRect.x = World.mouseX;
+
+
+
+  if(movingRect.x - fixedRect.x < movingRect.width/2 + fixedRect.width/2
+    && fixedRect.x - movingRect.x < movingRect.width/2 + fixedRect.width/2 && movingRect.y - fixedRect.y < movingRect.height/2 + fixedRect.height/2
+    && fixedRect.y - movingRect.y < movingRect.height/2 + fixedRect.height/2){
+    movingRect.shapeColor = "red"
+    fixedRect.shapeColor = "red"
+  }
+
+  else{
+    fixedRect.shapeColor = "green"
+    movingRect.shapeColor = "green"
+  }  
+
+  
+  
+  
+  drawSprites();
+}
